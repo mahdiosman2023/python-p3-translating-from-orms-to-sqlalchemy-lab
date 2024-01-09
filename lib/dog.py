12e81628695cf061b0f91d0b8dc3465fa92812a7
mahdiosman2023 <mahdiosmanu78@gmail.com>
2024-01-09 11:15:29 +0300

    translating

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,85 @@
+from sqlalchemy import create_engine
 from models import Dog
 
+engine = create_engine('sqlite:///:memory:')
+
 def create_table(base):
-    pass
+    sql = """
+        CREATE TABLE IF NOT EXISTS dogs (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+        );
+    """
+
+    return engine
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+
+    return session
+
+def new_from_db(session, row):
+    return session.query(Dog).filter_by(id = row.id).first()
 
 def get_all(session):
-    pass
+    return session.query(Dog)
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter_by(name = name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter_by(id = id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter_by(name = name, breed = breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
+    return session
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from models import Dog
+
+# def create_table(base):
+#     pass
+
+# def save(session, dog):
+#     pass
+
+# def get_all(session):
+#     pass
+
+# def find_by_name(session, name):
+#     pass
+
+# def find_by_id(session, id):
+#     pass
+
+# def find_by_name_and_breed(session, name, breed):
+#     pass
+
+# def update_breed(session, dog, breed):
+#     pass
